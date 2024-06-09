335f87aba1bfac7c0b3a72245090e200185c15ce
伊藤 滉祐 <kosuke@pepabo.com>
2024-06-09 15:00:53 +0900

    fix

--- main.py
+++ main.py
@@ -4,20 +4,15 @@ import pyloudnorm as pyln
 import soundfile as sf
 import io
 
-# MP3ファイルのパス
 mp3_path = '/path/to/your/file.mp3'
 
-# MP3をWAVに変換し、メモリ上に保持
 audio = AudioSegment.from_file(mp3_path, format='mp3')
 wav_io = io.BytesIO()
 audio.export(wav_io, format='wav')
-wav_io.seek(0)  # バイトオブジェクトの先頭に戻る
+wav_io.seek(0)
 
-# バイトオブジェクトを使ってWAVデータをsf.readで読み込む
 data, sample_rate = sf.read(wav_io)
-
-# LUFS解析
-meter = pyln.Meter(sample_rate)  # BS.1770メーターを作成
+meter = pyln.Meter(sample_rate)
 loudness = meter.integrated_loudness(data)
 
 print(f'Integrated Loudness (LUFS): {loudness}')
